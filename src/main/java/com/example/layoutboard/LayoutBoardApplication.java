38d6961e0f8ee0f9b386aa743e9d4a9c63f3bdae
gksansrjf <wandan531@qq.com>
2024-07-10 12:05:52 +0900

    작업틀

---
+++ src/main/java/com/example/layoutboard/LayoutBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.layoutboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayoutBoardApplication.class, args);
+    }
+
+}
